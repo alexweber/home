5c327bb20a9029c8edc91e1e1b846486812b00ae
Alex Weber <alexweber15@gmail.com>
2014-12-10 21:01:43 -0200

    initial .brew.sh homebrew installation script

---
+++ .brew.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.install.sh` has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# @TODO Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+#brew install moreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+# Install Bash 4.
+brew install bash
+brew install bash-completion
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some OS X tools.
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/screen
+
+# Install other useful binaries.
+brew install ack
+brew install autoconf
+brew install automake
+brew install bfg
+brew install composer
+brew install dtrx
+brew install gettext
+brew install git
+brew install hub
+brew install libpng
+brew install libsass
+brew install homebrew/php/PHP53
+brew install qcachegrind
+brew install rename
+# brew install solr36
+brew install tree
+brew install zopfli
+
+# Install Node.js. Note: this installs `npm` too, using the recommended
+# installation method.
+brew install node
+
+# Remove outdated versions from the cellar.
+brew cleanup
