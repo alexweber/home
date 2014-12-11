5b568a5f17c4bd6ebe8fe91cca62c5cfa327ba33
Alex Weber <alexweber15@gmail.com>
2014-12-11 00:05:22 -0200

    tweak code comments in .cask.sh

--- .cask.sh
+++ .cask.sh
@@ -24,9 +24,9 @@ brew tap caskroom/versions
 brew cask install alfred
 brew cask install atom
 brew cask install appzapper
-#brew cask install boom
+# brew cask install boom
 brew cask install ccleaner
-#brew cask install dash
+# brew cask install dash
 brew cask install dosbox
 brew cask install dropbox
 brew cask install firefox
@@ -34,37 +34,37 @@ brew cask install google-chrome
 brew cask install google-chrome-canary
 brew cask install harvest
 brew cask install hipchat
-#brew cask install imagealpha
-#brew cask install imageoptim
+# brew cask install imagealpha
+# brew cask install imageoptim
 brew cask install iterm2
 brew cask install java
 brew cask install joinme
 brew cask install kaleidoscope
-#brew cask install knox
+# brew cask install knox
 brew cask install lastfm
 brew cask install libreoffice
 brew cask install linkinus
-#brew cask install mamp
-#brew cask install miro-video-converter
+# brew cask install mamp
+# brew cask install miro-video-converter
 brew cask install minco
 brew cask install mmex
 brew cask install moom
-#brew cask install multidoge
-#brew cask install mysqlworkbench
+# brew cask install multidoge
+# brew cask install mysqlworkbench
 brew cask install onepassword
 brew cask install phpstorm
-#brew cask install screenflow
+# brew cask install screenflow
 brew cask install sequel-pro
 brew cask install skype
 brew cask install smcfancontrol
-#brew cask install sparrow
-#brew cask install steam
+# brew cask install sparrow
+# brew cask install steam
 brew cask install sublime-text
 brew cask install torbrowser
 brew cask install tower
 brew cask install transmission
 brew cask install transmit
-#brew cask install truecrypt
+# brew cask install truecrypt
 brew cask install vagrant
 brew cask install virtualbox
 brew cask install vlc
