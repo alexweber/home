792f7bac92c9726cda6bbf8a946dd41315dea0cb
Alex Weber <alexweber15@gmail.com>
2014-12-13 09:46:50 -0200

    initial version of osx.sh script

---
+++ osx.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` timestamp until we're done.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install homebrew.
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install homebrew packages.
+source .brew.sh
+
+# Install homebrew casks.
+source .cask.sh
+
+# Install Bash 4.
+echo "/usr/local/bin/bash" | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/bash
+
+# @TODO stuff from .osx
