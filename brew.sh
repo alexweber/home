aaeec72f810b93d75302d6fdd0309bed1ff32a92
Alex Weber <alexweber15@gmail.com>
2015-01-19 19:11:47 -0200

    override system vi with vim

--- brew.sh
+++ brew.sh
@@ -39,6 +39,7 @@ brew install homebrew/versions/bash-completion2
 brew install wget --with-iri
 
 # Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
