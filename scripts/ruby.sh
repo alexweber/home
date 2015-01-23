2610f6ec849c70b8929e0fcc4ef0b5f9b7524a45
Alex Weber <alexweber15@gmail.com>
2015-01-23 12:45:39 -0200

    also update gems when running ruby script

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -8,5 +8,9 @@ sudo -v
 # Keep-alive: update existing `sudo` timestamp until we're done.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-sudo gem install bundle
+# Install our global gems.
+sudo gem install bundler
 sudo gem install mailcatcher
+
+# Update installed gems.
+sudo gem update
