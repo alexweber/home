78925b695ed58836532da0812215af7ae6c58feb
Alex Weber <alexweber15@gmail.com>
2015-01-23 10:47:59 -0200

    install phpunit with composer

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Composer.
+# Install command-line tools globally using Composer.
 
 composer self-update
 composer global require drush/drush:dev-master
+composer global require "phpunit/phpunit=4.4.*"
 composer global install
