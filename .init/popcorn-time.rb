237284c5e112760563b8c28f7e887b9c021d4976
Alex Weber <alexweber15@gmail.com>
2015-01-23 17:25:21 -0200

    tweak popcorn time cask

--- .init/popcorn-time.rb
+++ .init/popcorn-time.rb
@@ -4,8 +4,8 @@ cask :v1 => 'popcorn-time' do
   sha256 'ef21b6db873ee173883390bfc83204aa28b869ee24545cc0b6706152f46710ed'
 
   # This is a redirect URL.
-  # url 'https://get.popcorntime.io/build/Popcorn-Time-0.3.7.1-Mac.dmg'
-  url 'http://popcorn.obsidian.goender.net/build/Popcorn-Time-0.3.7.1-Mac.dmg'
+  # url 'https://get.popcorntime.io/build/Popcorn-Time-#{version}-Mac.dmg'
+  url 'http://popcorn.obsidian.goender.net/build/Popcorn-Time-#{version}-Mac.dmg'
   name 'Popcorn Time'
   homepage 'https://popcorntime.io/'
   license :gpl
