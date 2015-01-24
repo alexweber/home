49b0ec92196c5521a6e405e8fe05cda6b8fd3112
Alex Weber <alexweber15@gmail.com>
2015-01-24 09:37:57 -0200

    tentatively dont ask for sudo in home script because each sub-script that requires it will do so anyway

--- scripts/home.sh
+++ scripts/home.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Ask for the administrator password upfront.
-sudo -v
+# sudo -v
 
 # Keep-alive: update existing `sudo` timestamp until we're done.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Setup dotfiles.
 source dotfiles.sh
