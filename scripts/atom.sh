015b1a70e3ec35bfd91132dbcde350fd3ed8c6d1
Alex Weber <alexweber15@gmail.com>
2015-02-05 14:32:27 -0200

    add a bunch of extra atom plugins

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
 # Install Atom packages.
+apm install atom-beautify
 apm install autocomplete-plus
 apm install color-picker
 apm install dash
+apm install editorconfig
+apm install git-control
+apm install git-tab-status
 apm install isotope-ui
 apm install linter
+apm install linter-jshint
+apm install linter-php
+apm install linter-phpcs
+apm install linter-scss-lint
+apm install local-history
 apm install minimap
+apm install minimap-git-diff
 apm install monokai
+apm install project-manager
 apm install seti-syntax
 apm install seti-ui
 apm install todo-show
