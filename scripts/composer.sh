99c185f73d5eb2185ac618b6635f0076b2438a51
Alex Weber <alexweber15@gmail.com>
2015-02-10 08:51:27 -0200

    install behat, mink and friends using composer

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -3,6 +3,9 @@
 # Install command-line tools globally using Composer.
 
 composer self-update
-composer global require drush/drush:dev-master
+composer global require "drush/drush:dev-master"
 composer global require "phpunit/phpunit=4.4.*"
+composer global require "behat/mink": "*"
+composer global require "behat/mink-goutte-driver": "*"
+composer global require "behat/mink-selenium2-driver": "*"
 composer global install
