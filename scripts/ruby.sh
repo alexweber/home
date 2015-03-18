2c887a023b0702de7a72bfe0b0b7eb2cad7f858c
Alex Weber <alexweber15@gmail.com>
2015-03-18 09:32:59 -0300

    update gems before installing

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -8,9 +8,9 @@ sudo -v
 # Keep-alive: update existing `sudo` timestamp until we're done.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Update installed gems.
+sudo gem update --system
+
 # Install our global gems.
 sudo gem install bundler
 sudo gem install mailcatcher
-
-# Update installed gems.
-sudo gem update
