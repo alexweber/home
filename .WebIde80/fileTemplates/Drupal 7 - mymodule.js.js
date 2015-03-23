f5a5d5c573dc1a34093683a002136503bcc936e4
Alex Weber <alexweber15@gmail.com>
2015-03-23 08:38:55 -0300

    added phpstorm/idea settings with drupal stuff

---
+++ .WebIde80/fileTemplates/Drupal 7 - mymodule.js.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Documentation missing.
+ */
+
+(function ($) {
+  'use strict';
+  Drupal.behaviors.${FILE_NAME} = {
+    attach: function (context, settings) {
+      // Do something.
+    }
+  };
+}(jQuery));
