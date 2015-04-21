12bd18892938005b550585414db23fa96b9e4dbf
Alex Weber <alexweber15@gmail.com>
2015-04-21 11:52:14 -0300

    update global packages as well in composer.sh
    keep it consistent with the others

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -3,6 +3,7 @@
 # Install command-line tools globally using Composer.
 
 composer self-update
+composer global update
 composer global require "drush/drush:dev-master"
 composer global require "phpunit/phpunit=4.4.*"
 composer global require "behat/mink": "*"
