a15e63e70684556ada7b1a5ccf02390a4dadbb66
Alex Weber <alexweber15@gmail.com>
2015-04-27 12:30:50 -0300

    fix composer script

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -3,7 +3,6 @@
 # Install command-line tools globally using Composer.
 
 composer self-update
-composer global update
 composer global require "drush/drush:dev-master"
 composer global require "phpunit/phpunit=4.4.*"
 composer global require "behat/mink": "*"
@@ -12,3 +11,5 @@ composer global require "behat/mink-selenium2-driver": "*"
 composer global require "squizlabs/php_codesniffer=*"
 composer global require "drupal/coder:\>7"
 composer global install
+
+composer global update
