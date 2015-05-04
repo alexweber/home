8fe359dbc65d3178999bc649961b3d0494d4479d
Alex Weber <alexweber15@gmail.com>
2015-05-04 14:15:52 -0300

    fix behat installation via composer

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -5,9 +5,10 @@
 composer self-update
 composer global require "drush/drush:dev-master"
 composer global require "phpunit/phpunit=4.4.*"
-composer global require "behat/mink": "*"
-composer global require "behat/mink-goutte-driver": "*"
-composer global require "behat/mink-selenium2-driver": "*"
+composer global require "behat/behat=*"
+composer global require "behat/mink=*"
+composer global require "behat/mink-goutte-driver=*"
+composer global require "behat/mink-selenium2-driver=*"
 composer global require "squizlabs/php_codesniffer=*"
 composer global require "drupal/coder:\>7"
 composer global install
