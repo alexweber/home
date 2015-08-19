c352b75ae8733576766aa2a18d31556e47d617f3
Alex Weber <alexweber15@gmail.com>
2015-08-19 16:51:30 -0300

    update node modules

--- scripts/node.sh
+++ scripts/node.sh
@@ -8,13 +8,21 @@ sudo -v
 # Keep-alive: update existing `sudo` timestamp until we're done.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+sudo npm install -g add-cors-to-couchdb
+sudo npm install -g aurelia-cli
+sudo npm install -g babel
 sudo npm install -g bower
 sudo npm install -g bower-update
 #sudo npm install -g browserify
 sudo npm install -g cordova
+sudo npm install -g electron-prebuilt
 sudo npm install -g eslint
+sudo npm install -g express
+sudo npm install -g express-generator
 sudo npm install -g firebase-tools
 sudo npm install -g fixmyjs
+sudo npm install -g generator-aurelia
+sudo npm install -g generator-electron
 # sudo npm install -g gitbanner
 sudo npm install -g grunt-cli
 # sudo npm install -g gitgoodies
@@ -22,30 +30,26 @@ sudo npm install -g gulp
 sudo npm install -g ionic
 sudo npm install -g ios-deploy
 sudo npm install -g ios-sim
+sudo npm install -g istanbul
+sudo npm install -g jpegtran
+sudo npm install -g jsdoc
 sudo npm install -g jshint
 sudo npm install -g jspm
 sudo npm install -g karma-cli
+sudo npm install -g knex
+sudo npm install -g learnyounode
+sudo npm install -g mocha
 sudo npm install -g node-gyp
 sudo npm install -g nodemon
 sudo npm install -g npm-check-updates
-# sudo npm install -g phonegap
+sudo npm install -g pixijs-cli
+sudo npm install -g optipng-bin
 sudo npm install -g protractor
-sudo npm install -g typescript
+sudo npm install -g run-scripts
 sudo npm install -g scss-lint
+sudo npm install -g semver
 sudo npm install -g slush
+sudo npm install -g typescript
+sudo npm install -g uglify-js
+sudo npm install -g webpack
 sudo npm install -g yo
-
-# Yeoman generators.
-# sudo npm install -g generator-angular
-# sudo npm install -g generator-angularjs-cordova
-# sudo npm install -g generator-atom
-# sudo npm install -g generator-generator
-# sudo npm install -g generator-gulp-angular
-# sudo npm install -g generator-gulp-angularfire
-# sudo npm install -g generator-gulp-bootstrap
-# sudo npm install -g generator-hedley
-# sudo npm install -g generator-ionic
-# sudo npm install -g generator-nm
-# sudo npm install -g generator-node-express
-# sudo npm install -g generator-symfony
-# sudo npm install -g generator-webapp
